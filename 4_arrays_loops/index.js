c0d961e08c677e9d07f4ae466ad25b68b5c196c4
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-05-29 16:52:21 +0100

    more code

--- 4_arrays_loops/index.js
+++ 4_arrays_loops/index.js
@@ -1,26 +1,46 @@
-let coffeeOrder = [
-  "alex - cortardo",
-  "ben - cortado",
-  "charlie - whatevers new",
-];
+// let coffeeOrder = [
+//   "alex - cortardo",
+//   "ben - cortado",
+//   "charlie - whatevers new",
+// ];
 
-// console.log(coffeeOrder[2]);
+// // console.log(coffeeOrder[2]);
 
-coffeeOrder[1] = "ann - vanilla latte";
+// coffeeOrder[1] = "ann - vanilla latte";
 
+// // console.log(coffeeOrder.length);
+
+// // const arr = [1, 2, 3, 4];
+
+// // console.log(arr.length);
+// console.log(coffeeOrder);
+// coffeeOrder.push("michael - triple espresso");
+
+// console.log(coffeeOrder);
 // console.log(coffeeOrder.length);
 
-// const arr = [1, 2, 3, 4];
+// coffeeOrder.pop();
+// console.log(coffeeOrder);
 
-// console.log(arr.length);
-console.log(coffeeOrder);
-coffeeOrder.push("michael - triple espresso");
+// coffeeOrder.pop();
+// console.log(coffeeOrder);
 
-console.log(coffeeOrder);
-console.log(coffeeOrder.length);
+const favDrinks = ["sprite", "coke", "tea"];
 
-coffeeOrder.pop();
-console.log(coffeeOrder);
+// console.log(favDrinks[0]);
+// console.log(favDrinks[1]);
+// console.log(favDrinks[2]);
 
-coffeeOrder.pop();
-console.log(coffeeOrder);
+for (let i = 0; i < favDrinks.length; i++) {
+  console.log(favDrinks[i]);
+}
+
+const oddNums = [];
+
+for (let i = 0; i < 20; i++) {
+  if (i % 2 != 0) {
+    oddNums.push(i);
+  }
+}
+
+console.log(oddNums);
