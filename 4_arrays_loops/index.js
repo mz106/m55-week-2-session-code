08749a6f868f880038858c20e5ab3af8206e81e6
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-05-30 11:13:31 +0100

    while loop code

--- 4_arrays_loops/index.js
+++ 4_arrays_loops/index.js
@@ -25,22 +25,62 @@
 // coffeeOrder.pop();
 // console.log(coffeeOrder);
 
-const favDrinks = ["sprite", "coke", "tea"];
+// const favDrinks = ["sprite", "coke", "tea"];
 
-// console.log(favDrinks[0]);
-// console.log(favDrinks[1]);
-// console.log(favDrinks[2]);
+// // console.log(favDrinks[0]);
+// // console.log(favDrinks[1]);
+// // console.log(favDrinks[2]);
 
-for (let i = 0; i < favDrinks.length; i++) {
-  console.log(favDrinks[i]);
+// for (let i = 0; i < favDrinks.length; i++) {
+//   console.log(favDrinks[i]);
+// }
+
+// const oddNums = [];
+
+// for (let i = 0; i < 20; i++) {
+//   if (i % 2 != 0) {
+//     oddNums.push(i);
+//   }
+// }
+
+// console.log(oddNums);
+
+// let age = 15;
+
+// while (age < 18) {
+//   console.log("Youre a child!");
+//   age++;
+// }
+
+// console.log("Youre an adult");
+
+const ranNum = Math.random();
+
+// console.log(`ranNum: ${ranNum}`);
+
+const multipliedNum = ranNum * 4;
+
+// console.log(`multipliedNum (ranNum * 10): ${multipliedNum}`);
+
+const floorNum = Math.floor(multipliedNum);
+
+// console.log(`floorNum (multipliedNum floored): ${floorNum}`);
+
+// const myArr = ["hello", "world", "everyone"];
+
+// const myElement = myArr[1];
+
+const cards = ["diamond", "spade", "heart", "club"];
+let currentCard = "diamond";
+
+while (currentCard != "spade") {
+  console.log(currentCard);
+  // const randomNum = Math.random();
+  // const multipliedNum = randomNum * 4;
+  // const floorNum = Math.floor(multipliedNum);
+  currentCard = cards[Math.floor(Math.random() * 4)];
 }
 
-const oddNums = [];
+console.log("after while loop: ", currentCard);
 
-for (let i = 0; i < 20; i++) {
-  if (i % 2 != 0) {
-    oddNums.push(i);
-  }
-}
-
-console.log(oddNums);
+// console.log(Math.random());
